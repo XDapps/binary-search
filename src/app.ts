f8274d7540eaaee69ee2b03e2cdd9f78e872405a
XDapps <xdev@xdapps.xyz>
2024-04-04 20:52:54 -0400

    Removed Comma Delimited Example for Simplicity

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,12 @@ import { BinarySearch } from './models/BinarySearch';
 import { SQLite3 } from './models/SQLite3';
 
 //******* Binary Search with Text File Input Space Delimited********/
-const inputFilePathSpace = 'src/files/inputSpace.txt';
-const outputFilePathSpace = 'src/files/outputSpace.txt';
-const delimiterSpace = ' '; // Delimiter used to separate numbers in the file, " ", "," etc...
-const targetNumberSpace = 22; // The number you are going to search (key)
-const searcherSpaceDelimiter = new BinarySearch(targetNumberSpace, inputFilePathSpace, outputFilePathSpace, delimiterSpace);
-searcherSpaceDelimiter.search();
-
-//******* Binary Search with Text File Input Comma Delimited ********/
-const inputFilePathComma = 'src/files/inputComma.txt';
-const outputFilePathComma = 'src/files/outputComma.txt';
-const targetNumberComma = 7; // The number you are going to search (key)
-const delimiterComma = ','; // Delimiter used to separate numbers in the file, " ", "," etc...
-const searcherCommaDelimiter = new BinarySearch(targetNumberComma, inputFilePathComma, outputFilePathComma, delimiterComma);
-searcherCommaDelimiter.search();
+const inputFilePath = 'src/files/input.txt';
+const outputFilePath = 'src/files/output.txt';
+const delimiter = ' '; // Delimiter used to separate numbers in the file, " ", "," etc...
+const targetNumber = 22; // The number you are going to search (key)
+const searcher = new BinarySearch(targetNumber, inputFilePath, outputFilePath, delimiter);
+searcher.search();
 
 //****** Connect to the Database and Query Previous Searches ********/
 const db = new SQLite3();
